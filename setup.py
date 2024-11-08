1f3069361e07298955795847ed50ae7b40192216
arshdeep6822 <deeparsh618@gmail.com>
2024-11-08 22:17:01 +0530

    download the chatbot package and other requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chatbot',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+        'faiss-cpu',
+        'sentence-transformers',
+        'torch',
+        'bs4',
+        'langchain',
+        'langchain_community',
+        'openai',
+        'python-dotenv'
+    ],
+    author='Arshdeep Singh',
+    author_email='deeparsh618@gmail.com',
+    description='A chatbot powered by WordPress content',
+    url='https://github.com/arshdeep6822/arshdeep_singh_wasserstoff_AiTask',
+)
