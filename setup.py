35267526925c1cb5b8c5c0bce91d1e9cb97c0d64
Arshdeep Singh <deeparsh618@gmail.com>
2024-11-11 10:50:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='chatbot',
+    name='RAG CHATBOT FOR WORDPRESS WEBSITES',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
